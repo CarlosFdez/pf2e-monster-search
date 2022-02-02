218714119d68a8dd9c4e49167b77ffee02af55d4
CarlosFdez <CarlosFdez@users.noreply.github.com>
2022-02-02 12:31:06 +0000

    Deploying to gh-pages from @ CarlosFdez/pf2e-monster-search@8ee8b20c3f7aff1c19fbc6e56e67235e22bde88d 🚀

---
+++ _next/static/xUUYF3_XG7lwwK0ilJZDH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a1a2896184e8da59.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
